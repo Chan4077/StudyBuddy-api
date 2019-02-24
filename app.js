be8bda40ec98bb06aefc8dd9e6e976e25fd6827c
Edric Chan <edric.chan.1997@gmail.com>
2019-02-25 00:02:28 +0800

    Add title and favicon to server

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var responseTime = require('response-time');
+var favicon = require('serve-favicon');
+var path = require('path');
 var admin = require('firebase-admin');
 var port = process.env.PORT || 3000;
 var app = express();
 // This can be found from the service accounts tab in your Firebase app settings
 var serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT || require('./service-account-key.json');
 
+// Sets the document title
+// See https://stackoverflow.com/a/54356269/6782707 for more info
+app.set('title', 'StudyBuddy API');
+
 // Adds the X-Response-Time header to indicate how long a request took
 // See https://github.com/expressjs/response-time for more info
 app.use(responseTime());
+
+// Add a favicon
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 // Initialise a Firebase Admin SDK app
 admin.initializeApp({
@@ -32,5 +41,5 @@ require('./routes/uptime')(app);
 require('./routes/list')(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+  console.log(`App is listening on port ${port}.`);
 });
