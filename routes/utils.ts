251e3312dbc30d0984519e4e928d838701d355cc
Edric Chan <edric.chan.1997@gmail.com>
2019-03-04 23:48:37 +0800

    utils: Add support for sending status code w/ error response

--- routes/utils.ts
+++ routes/utils.ts
@@ -8,8 +8,10 @@ export class Utils {
    * Sends a JSON error
    * @param res A `Response` instance
    * @param error The error
+   * @param status The status of the request, if any
+   * @returns The instance of `res`
    */
-  public static sendJsonError(res: Response, error: any) {
-    return res.send(error);
+  public static sendJsonError(res: Response, error: any, status?: number): Response {
+    return status ? res.status(status).send(error) : res.send(error);
   }
 }
